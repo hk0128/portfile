8f90627dcdb84e5db51dbb83900d1fa71bed9bb7
hiro <hironobu.kamata@adglobe.co.jp>
2023-12-20 18:31:25 +0900

    react router用に修正

--- src/main.tsx
+++ src/main.tsx
@@ -6,14 +6,13 @@ import Top from './pages/Top'
 import Skill from './pages/Skill'
 import Work from './pages/Work'
 
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Top />} />
-        <Route path="/skill" element={<Skill />} />
-        <Route path="/work" element={<Work />} />
+        <Route path="/portfolio/" element={<Top />} />
+        <Route path="/portfolio/skill" element={<Skill />} />
+        <Route path="/portfolio/work" element={<Work />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
