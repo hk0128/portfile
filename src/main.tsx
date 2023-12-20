bbdb3c1ad56fddc293b9d4b7de38ed794c98f81e
hiro <hironobu.kamata@adglobe.co.jp>
2023-12-21 04:28:52 +0900

    修正

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import Work from './pages/Work'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Top />} />
         <Route path="/skill" element={<Skill />} />
